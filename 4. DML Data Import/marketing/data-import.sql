fdec1a6b0187b880e1825b205f82b35a912d859f
sanskar55g <152682307+sanskar55g@users.noreply.github.com>
2026-06-24 23:54:03 +0530

    Update file paths in data import SQL script

--- 4. DML Data Import/marketing/data-import.sql
+++ 4. DML Data Import/marketing/data-import.sql
@@ -1,20 +1,18 @@
 -- Load raw marketing funnel CSVs into staging tables
 -- No transformations, everything loaded as VARCHAR
 
-LOAD DATA INFILE '<PATH TO FILE olist_marketing_qualified_leads_dataset.csv>'
+LOAD DATA INFILE 'PATH TO olist_marketing_qualified_leads_dataset.csv'
+IGNORE
 INTO TABLE marketing.marketing_qualified_leads_staging
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(mql_id, first_contact_date, landing_page_id, origin);
+IGNORE 1 ROWS;
 
-LOAD DATA INFILE '<PATH TO FILE olist_marketing_closed_deals_dataset.csv>'
+LOAD DATA INFILE 'PATH TO olist_closed_deals_dataset.csv'
+IGNORE
 INTO TABLE marketing.marketing_closed_deals_staging
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(mql_id, seller_id, sdr_id, sr_id, won_date, business_segment, business_type, 
-lead_type, lead_behaviour_profile, has_company, has_gtin, average_stock, 
-declared_product_catalog_size, declared_monthly_revenue);
+IGNORE 1 ROWS;
